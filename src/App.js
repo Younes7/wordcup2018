ff16c2a1be7d2c84c4526be9d5317f435677170f
drameBoubacar <boubacardrame9@gmail.com>
2018-06-18 11:10:08 +0100

    ajout de json-server et modif de la banniere

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Banner from './components/Banner';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>Worl cup</p>
-        </header>
+        <Banner/>
       </div>
     );
   }
---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Banner extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <p>Worl cup</p>
+        </header>
+      </div>
+    )
+  }
+} 
