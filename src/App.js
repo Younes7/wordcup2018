e83db9e648129dd3fe34821fc955fc3e6ec744a4
drameBoubacar <boubacardrame9@gmail.com>
2018-06-18 12:16:40 +0100

    Ajout du carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Banner from './components/Banner';
+import Slider from './components/Slider';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Banner/>
+        <Slider/>
       </div>
     );
   }
